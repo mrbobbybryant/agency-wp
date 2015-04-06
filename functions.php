5b0860d966e8da7b8f251f8e3621f13775e1e854
Bobby Bryant <bobby@hatrackmedia.com>
2015-04-06 05:40:34 -0400

    adding and removing styles and scripts.

--- functions.php
+++ functions.php
@@ -46,7 +46,7 @@ function agency_wp_setup() {
 	 *
 	 * @link http://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
 	 */
-	//add_theme_support( 'post-thumbnails' );
+	add_theme_support( 'post-thumbnails' );
 
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus( array(
@@ -100,11 +100,26 @@ add_action( 'widgets_init', 'agency_wp_widgets_init' );
  * Enqueue scripts and styles.
  */
 function agency_wp_scripts() {
+
+	wp_enqueue_style( 'normalize-css', get_template_directory_uri() . '/css/normalize.css', array(), '3.0.0' );
+	wp_enqueue_style( 'elegant-icons', get_template_directory_uri() . '/css/elegrant-icons.css', array(), '20150405' );
+	wp_enqueue_style( 'responsive-css', get_template_directory_uri() . '/css/responsive.css', array(), '20150405' );
+	wp_enqueue_style( 'flexslider', get_template_directory_uri() . '/css/flexslider.css', array(), '20150405' );
+
+	wp_enqueue_script( 'modernizr', get_template_directory_uri() . '/js/vendor/modernizr.js', array(), '2.7.2', true );
+	wp_enqueue_script( 'fastclick', get_template_directory_uri() . '/js/vendor/fastclick.js', array(), '1.0.0', true );
+	wp_enqueue_script( 'placeholder', get_template_directory_uri() . '/js/vendor/placeholder.js', array(), '2.0.7', true );
+
+
+	// Foundation Styles Scripts.
+	wp_enqueue_script( 'foundation-js', get_template_directory_uri() . '/js/foundation.min.js', array('jquery'), '20150405', true );
+	wp_enqueue_style( 'foundation-css', get_template_directory_uri() . '/css/foundation.css', array(), '20150405' );
+	
 	wp_enqueue_style( 'agency_wp-style', get_stylesheet_uri() );
-
-	wp_enqueue_script( 'agency_wp-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
-
-	wp_enqueue_script( 'agency_wp-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+	wp_enqueue_script( 'agency_wp-js', get_template_directory_uri() . '/scripts.js', array('jquery'), '20150405', true );
+	
+	wp_enqueue_script( 'agency_wp-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20150405', true );
+	wp_enqueue_script( 'agency_wp-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20150405', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
